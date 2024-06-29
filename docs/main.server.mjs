b7bc0c4a3795b41c0ea4f8f9e76fee3a7f525588
ziadAlattar <ziadalattar22@gmail.com>
2024-06-29 05:45:06 +0300

    4commit

---
+++ docs/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-Y76AQ34S.mjs";import"./chunk-HLH3LDHY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-DLU7JNAG.mjs";import"./chunk-HLH3LDHY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
