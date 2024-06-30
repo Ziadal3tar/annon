d32aa19b1fbcc11c514a3bd5ebdd8fb349ded3c0
ziadAlattar <ziadalattar22@gmail.com>
2024-07-01 02:51:54 +0300

    'responsiv'

--- docs/main.server.mjs
+++ docs/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-Y76AQ34S.mjs";import"./chunk-HLH3LDHY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-HXASQY2P.mjs";import"./chunk-HLH3LDHY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
